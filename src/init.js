89dd4087907f4b335ab0c72ace5ccf27bb4aa054
BHARATH CHANDRA <bharathchandra541@gmail.com>
2021-08-04 19:22:50 +0530

    init.js : use animationframes to handle events :sparkles:

---
+++ src/init.js
@@ -0,0 +1,32 @@
+/* 
+  Ideally , init.js would contain all the fluffy stuff like google analytics etc.. 
+  which just runs in the browser and interacts with the tirdparty or browser api.
+
+  In this case I will be using it to initialize the timer which decides a new activity for the FOX.
+*/
+const TICK_RATE = 5000;
+
+function tick() {
+    console.log('tick', Date.now());
+}
+
+async function init() {
+
+    let nexttimeToTick = Date.now();
+
+    function nextAnimationFrame() {
+
+        const now = Date.now();
+        if (nexttimeToTick <= now) {
+            tick();
+            nexttimeToTick = now + TICK_RATE;
+        }
+
+        requestAnimationFrame(nextAnimationFrame);
+    }
+
+    nextAnimationFrame();
+
+}
+
+init();
